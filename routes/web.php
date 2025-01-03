6ae4d53be1a936463a16468dbcaa848bd5372f5e
gustavojuan <gustavojuan@gmail.com>
2025-01-03 18:11:52 +0100

    feat(project): a_user_can_create_aproject

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\ProjectsController;
+use App\Models\Project;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/projects', [ProjectsController::class, 'index']);
+Route::post('/projects', [ProjectsController::class, 'store']);
